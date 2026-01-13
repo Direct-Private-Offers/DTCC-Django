83cba63ef09311cd46240ca3302ec620c010ba71
Arthur Smitheman <adsco1234@gmail.com>
2026-01-13 14:48:26 -0500

    Remove apps.core to avoid database dependencies

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_spectacular',
-    'apps.core',
+    # 'apps.core',
     'apps.euroclear',
     'apps.issuance',
     'apps.derivatives',
